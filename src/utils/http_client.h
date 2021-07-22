a77fb392784377c1c0d25bcc5728b30df7970eb7
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2021-07-22 21:27:33 +0200

    Added follow redirect and compressio for http client.

--- src/utils/http_client.h
+++ src/utils/http_client.h
@@ -13,6 +13,10 @@ inline httplib::Result do_request(const std::string &uri) {
     std::string req_path = u.path + "?" + u.query;
 
     httplib::Client cli(Fmt("%s://%s:%d", u.protocol.c_str(), u.domain.c_str(), u.port).c_str());
+    
     cli.enable_server_certificate_verification(false);
+    cli.set_follow_location(true);
+    cli.set_compress(true);
+
     return cli.Get(req_path.c_str());
 }
