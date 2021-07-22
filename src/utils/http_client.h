8560a67c968e53f51be75f88294fdfabf32b4d4a
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2021-07-22 16:55:11 +0200

    Modified http_client to skip ssl certificate verification (otherwise github requests fail).

--- src/utils/http_client.h
+++ src/utils/http_client.h
@@ -8,9 +8,11 @@
 #include <utils/http_url.h>
 #include <utils/strings.h>
 
-httplib::Result do_request(const std::string &uri) {
+inline httplib::Result do_request(const std::string &uri) {
     HTTPURL u(uri);
     std::string req_path = u.path + "?" + u.query;
 
-    return httplib::Client(Fmt("%s://%s:%d", u.protocol.c_str(), u.domain.c_str(), u.port).c_str()).Get(req_path.c_str());
+    httplib::Client cli(Fmt("%s://%s:%d", u.protocol.c_str(), u.domain.c_str(), u.port).c_str());
+    cli.enable_server_certificate_verification(false);
+    return cli.Get(req_path.c_str());
 }
